b78f0fb26e6036ce3b58107a36e11b84d00167b8
ivan <ivanventik@gmail.com>
2026-01-29 15:44:15 +0200

    added movies schemas & routes

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,33 @@
-# Write your code here
+from typing import Optional
+from pydantic import BaseModel, ConfigDict
+from datetime import date
+
+
+class MovieBase(BaseModel):
+    id: int
+    name: str
+    date: date
+    score: float
+    genre: str
+    overview: str
+    crew: str
+    orig_title: str
+    status: str
+    orig_lang: str
+    budget: int
+    revenue: float
+    country: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class MovieDetailResponseSchema(MovieBase):
+    pass
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: list[MovieDetailResponseSchema]
+    prev_page: Optional[str]
+    next_page: Optional[str]
+    total_pages: int
+    total_items: int
